448194263e0720cd57d466e8d04a0e0d97a16bfa
sdhani <shania.dhani46@myhunter.cuny.edu>
2020-05-15 20:24:17 -0400

    added env var to next config, priority

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
-require('dotenv').config()
+require("dotenv").config();
 module.exports = {
   env: {
     MONGO_URI: process.env.NEXT_EXAMPLE_MONGO_DBURI,
-  },
-}
+    NEXT_EXAMPLE_BASE_URL: process.env.NEXT_EXAMPLE_BASE_UR
+  }
+};
