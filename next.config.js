dd793ec04dda1f1414944ce6903dd2008d42f047
sdhani <shania.dhani46@myhunter.cuny.edu>
2020-05-15 20:27:52 -0400

    reverted change

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 module.exports = {
   env: {
-    MONGO_URI: process.env.NEXT_EXAMPLE_MONGO_DBURI,
-    NEXT_EXAMPLE_BASE_URL: process.env.NEXT_EXAMPLE_BASE_URL
+    MONGO_URI: process.env.NEXT_EXAMPLE_MONGO_DBURI
   }
 };
